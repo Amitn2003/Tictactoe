61b49480c7ae1358e665d5a4c9561343e84f9b3a
Amit <amitn1909@gmail.com>
2022-05-14 20:35:46 +0530

    Playing with bot introdused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from sys import argv
-
-
+import random
 
 
 def input_validate(int):
@@ -8,15 +7,17 @@ def input_validate(int):
         # Input validation "Success"
         if xState[int] == 0 and zState[int] == 0 :
             return 1
+        elif bot == 1:
+            return 0
         else:
-            print(f"You've entered {int}!\nDon't try to Change privious inputs! ")
+            print(f"You've entered {int}!\nDon't try to Change privious inputs!")
             return 0
     else:
         return 0
 
 
 def structure(xState, zState):
-    # zero = "X" if xState[0] != 0  else ("O" if zState[0] != 0 else 0)
+    
     zero = 'X' if xState[0] else ('O' if zState[0] else 0)
     one = 'X' if xState[1] else ('O' if zState[1] else 1)
     two = 'X' if xState[2] else ('O' if zState[2] else 2)
@@ -26,11 +27,14 @@ def structure(xState, zState):
     six = 'X' if xState[6] else ('O' if zState[6] else 6)
     seven = 'X' if xState[7] else ('O' if zState[7] else 7)
     eight = 'X' if xState[8] else ('O' if zState[8] else 8)
+
+    # Printing proper structure
     print(f" {zero} | {one} | {two} ")
     print(f"---|---|---")
     print(f" {three} | {four} | {five} ")
     print(f"---|---|---")
     print(f" {six} | {seven} | {eight} ")
+
     pass
 
 
@@ -101,7 +105,7 @@ if __name__ == "__main__":
     zState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     try :
-        print(f"Welcome {argv[1]}")
+        print(f"Welcome {argv[1].capitalize()}")
         user_name = argv[1]
     except :
         user_name = input("\nEnter your name: ")
@@ -111,6 +115,14 @@ if __name__ == "__main__":
     turn = 0    # Turn 0 is X's turn & 1 is O's turn
 
     chance_left = 9 #This is how many chance left for X & Y to continue play
+
+    bot_ask = input("Do you want to play with bot ? \nPress Y/y/1/Yes for yes \nPress N/n/0/No for play with another player : ")
+
+    if bot_ask[0] == "Y" or bot_ask[0] == "y" or bot_ask[0] == "1" or bot_ask[0] == "Yes" or bot_ask[0] == "True":
+        bot = 1
+    else:
+        bot = 0
+    
 
     print(f"\nHello {user_name}! Enjoy this game... \n ")
     
@@ -132,13 +144,19 @@ if __name__ == "__main__":
             else:
                 print("Enter valid input!!")
         else :
-            z_input = int(input("Enter Z's Input: "))
+            if bot == 1:
+                z_input = int(random.randrange(0,8))
+                print(f"Z's input is {z_input} \n")
+            else:
+                z_input = int(input("Enter Z's Input: "))
             intt = input_validate(z_input)
             if intt:
                 change_state("Z", z_input)
                 structure(xState, zState)
                 turn = 0
                 chance_left -= 1
+            elif intt is False and bot == 1:
+                pass
             else:
                 print("Enter valid input!!")
             
